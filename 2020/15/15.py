e16594c61bbbd731fe04f075941d1fc03cf04561
Jack Cogdill <jackcogdill@users.noreply.github.com>
2020-12-14 23:58:49 -0800

    day 15 - 19s

--- 2020/15/15.py
+++ 2020/15/15.py
@@ -8,15 +8,11 @@ def play(S):
 
     for turn in range(1, len(nums) + 1):
         n = nums[turn - 1]
-        last[n] = last[n][1], turn
+        p = last[n] = 0, turn
 
     for turn in range(turn + 1, S + 1):
-        prev = n
-        if last[prev][0] == 0:
-            n = 0
-        else:
-            n = last[prev][1] - last[prev][0]
-        last[n] = last[n][1], turn
+        n = p[1] - (p[0] or p[1])
+        p = last[n] = last[n][1], turn
 
     return n
 
